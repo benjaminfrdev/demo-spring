8318c7683d006a8498dc34e31818e5dec41f308d
khoa <khoadev@gmail.com>
2022-12-21 13:30:16 +0700

    second commit

---
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class WebController {
+
+    @GetMapping(value = "/getString")
+    public String getRandomString(){
+        return "abc";
+    }
+
+}
